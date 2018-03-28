90dd4548bdb4220e4504a8d527f82837f3c44595
mdbyars <mdbyars@haverford.edu>
2018-03-27 23:31:20 -0400

    bri slep

--- Downloads/mdbyars_hw_7.py
+++ Downloads/mdbyars_hw_7.py
@@ -1,7 +1,7 @@
 from scipy.interpolate import interp1d 
 import numpy as np 
 from io import StringIO 
-from scipy import *
+from sympy import Derivative  
 #regexp = r"(\d+\.\d+E\+\d)"
 #xcol = np.fromregex('./camb_scalcls.dat')[0]
 
@@ -19,15 +19,16 @@ for i in range(2000):
 yspline = []
 xspline = []
 i = 0
-while i<2000:
+while i<=2000:
 	yspline = yspline+ [yval(i)]
 	xspline = xspline+ [xval(i)]
 	i+=10
 f = interp1d(xspline, yspline)
+k=0
 yinter = []
-while i<2000:
-	yinter += [f(x[i])]
-	
+while k<2000:
+	yinter += [f(x[k])]
+	k+=1	
 
 xnew = np.linspace(0, 10, num=41, endpoint=True)
 import matplotlib.pyplot as plt 
@@ -36,15 +37,14 @@ plt.show()
 ydif = []
 j=0
 while j<2000:
-	ydif += [y[j]-yspline[j/10]]
-	j+=10
-plt.plot(xspline, ydif)
-plt.yscale('log')
+	ydif += [y[j]-yinter[j]]
+	j+=1
+plt.plot(x, ydif)
 plt.show()
-print(ydif)
-#for i in range(len(y)):
-#	y[i] = derivative(lambda i: (y[i]/(x[i]**2 + x[i]))
-
+f = lambda i : i*2
+for i in range(len(y)):
+	#y[i] = Derivative(lambda i: (y[i]/(x[i]**2 + x[i])), x[i]) 
+	y[i] = Derivative(f, 2) 
 plt.plot(x, y)
 plt.xscale('log')
 plt.show()
